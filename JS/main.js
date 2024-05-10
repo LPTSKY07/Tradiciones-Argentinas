4983b8fedc3d9a1adc9f18ec4074f81e3294d8eb
MatiasBidone <77997890+MatiasBidone@users.noreply.github.com>
2024-05-10 11:47:11 -0300

    Update main.js
    Se actualizo el main con el codigo para el slider de la pagina principal
    
    Signed-off-by: MatiasBidone <77997890+MatiasBidone@users.noreply.github.com>

--- JS/main.js
+++ JS/main.js
@@ -2,46 +2,44 @@ let imagenes = [
     { 
         "url": "./imagenes/carrusel/choripan-bs-as.jpg",
         "texto": "Centro",
+        "descripcion": "Centro Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet dolorem earum accusantium maiores dolore libero mollitia voluptates quia id magnam laudantium, doloribus quidem voluptate provident possimus, eius consequuntur iusto harum?",
         "link":"./centro.html"
     },
     {
         "url": "./imagenes/carrusel/chipa-litoral.jpg",
         "texto": "Litoral",
+        "descripcion": "Litoral Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet dolorem earum accusantium maiores dolore libero mollitia voluptates quia id magnam laudantium, doloribus quidem voluptate provident possimus, eius consequuntur iusto harum?",
         "link":"./litoral.html"        
     },
     {
         "url": "./imagenes/carrusel/locro-norte.jpg",
         "texto": "Norte",
+        "descripcion": "Norte Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet dolorem earum accusantium maiores dolore libero mollitia voluptates quia id magnam laudantium, doloribus quidem voluptate provident possimus, eius consequuntur iusto harum?",
         "link":"./norte.html"
     },
     {
         "url": "./imagenes/carrusel/chivito-mendocino.jpg",
         "texto": "Cuyo",
+        "descripcion": "Cuyo Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet dolorem earum accusantium maiores dolore libero mollitia voluptates quia id magnam laudantium, doloribus quidem voluptate provident possimus, eius consequuntur iusto harum?",
         "link":"./cuyo.html"
     },
     {
         "url": "./imagenes/carrusel/trucha-patagonia.jpg",
         "texto": "Patagonia",
+        "descripcion": "Patagonia Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet dolorem earum accusantium maiores dolore libero mollitia voluptates quia id magnam laudantium, doloribus quidem voluptate provident possimus, eius consequuntur iusto harum?",
         "link":"./patagonia.html"
     }
 ];
 
-let atras = document.getElementById("atras");
-let adelante = document.getElementById("adelante");
-let imagen = document.getElementById("imagen-carrusel");
-let texto = document.getElementById("texto-carrusel");
-let actual = 0;
+let index = 1;
+let textoSlider = document.getElementById("texto-slider");
+let imagenSlider = document.getElementById("img-fondo-slider");
 
-atras.addEventListener('click', () => {
-    actual-=1;
-    if (actual==-1){actual = imagenes.length - 1}
-    imagen.innerHTML = `<a href="${imagenes[actual].link}"><img src="${imagenes[actual].url}" alt="locro"></a>`;
-    texto.innerHTML = `<a href="${imagenes[actual].link}"><h2>${imagenes[actual].texto}</h2></a>`;
-});
 
-adelante.addEventListener('click', () => {
-    actual+=1;
-    if (actual==imagenes.length){actual = 0}
-    imagen.innerHTML = `<a href="${imagenes[actual].link}"><img src="${imagenes[actual].url}" alt="locro"></a>`;
-    texto.innerHTML = `<a href="${imagenes[actual].link}"><h2>${imagenes[actual].texto}</h2></a>`;
-});
+
+setInterval(()=>{
+    if (index==imagenes.length){index=0}
+    textoSlider.innerHTML = `<h1>${imagenes[index].texto}</h1><p>${imagenes[index].descripcion}</p>`;
+    imagenSlider.innerHTML = `<img src="${imagenes[index].url}" alt="imagen fondo de region" style="animation-duration: 2500ms; animation-name: slidein;">`;
+    index++;
+}, 4500)
